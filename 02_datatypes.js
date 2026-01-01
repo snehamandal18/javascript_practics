e5f0748fb81c5267838bd8b5197eee1757440a7a
Sneha Mandal <144563264+snehamandal18@users.noreply.github.com>
2026-01-01 23:16:13 +0530

    Add files via upload

---
+++ 02_datatypes.js
@@ -0,0 +1,28 @@
+
+"use strict" // treat all JS code as newer version
+
+// alert( 3+3 ) // we are using node JS not browser
+//code readability that should be high 
+console.log(3+3)
+console.log("Sneha")
+
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string =>""
+// null => standalone value , we can assign value as null means empty.
+// undefined => value assign nhi hua hai
+// symbol => unique
+
+
+//object
+
+
+console.log(typeof "Sneha")
+console.log(typeof age)
+console.log(typeof null)//object
+console.log(typeof undefined) // undefined
+
