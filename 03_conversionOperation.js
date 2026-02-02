f308c5c4901451c6e1ec202a1e26f69c57194cec
Sneha Mandal <144563264+snehamandal18@users.noreply.github.com>
2026-02-02 23:54:55 +0530

    Add files via upload

---
+++ 03_conversionOperation.js
@@ -0,0 +1,33 @@
+
+let score = "33abc"
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueNumber = Number(score)
+//console.log(typeof valueNumber);
+//console.log(valueNumber);//NaN
+ 
+
+//"33" => 33
+// "33abc" => NaN(not a number)
+// true => 1 , false => 0
+
+let isLoggedIn = 1;
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+// 1 => true , 0 => false
+//"" => false
+// "Sneha" => true
+let str1 = "sneha"
+let str2 = "mandal"
+let str3 = str1 + str2;
+console.log(str3);
+console.log("1"+2)//o/p-12
+console.log(1+"2")//o/p-12
+console.log("1"+2+2)//o/p-122
+console.log(1+2+"2")//o/p-32
+
+
+
